0d21a34efad5b336b1449b7ad251cfb6bf3817c8
Kostiantyn Koval <kostiantyn.koval@gmail.com>
2018-03-05 18:16:19 +0200

    registration form validation in progress. done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit } from '@angular/core';
 import {toggleHeight} from "@app/animations";
 
 @Component({
--- src/app/components/registration-modal/registration-modal.component.ts
+++ src/app/components/registration-modal/registration-modal.component.ts
@@ -2,27 +2,30 @@ import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {toggleHeight} from "@app/animations";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
+
 @Component({
   selector: 'registration-modal',
   templateUrl: './registration-modal.component.html',
   styleUrls: ['./registration-modal.component.css'],
+  animations: [ toggleHeight ]
 })
 export class RegistrationModalComponent implements OnInit {
 
-  // @Input('showStatus') showStatus: string;
-  // @Output('close') close: EventEmitter<any> = new EventEmitter;
+  @Output('close') close: EventEmitter<any> = new EventEmitter;
   form: FormGroup;
   ips: string[];
   isMismatched: boolean;
   noIps: boolean;
+  isIpEmpty: boolean;
+  @Input('animState')animState: string;
 
   constructor(private fb: FormBuilder) {
     this.form = fb.group({
       name: ['', Validators.required ],
       email: ['', Validators.compose([ Validators.required, Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)])],
       ip: ['', Validators.pattern(/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/)],
-      password: ['', Validators.compose([ Validators.required, Validators.minLength(6), Validators.maxLength(16), Validators.pattern(/^[a-zA-Z]\w{6,16}$$/)]) ],
-      password_confirm: ['', Validators.compose([ Validators.required, Validators.pattern(/^[a-zA-Z]\w{6,16}$/)]) ],
+      password: ['', Validators.compose([ Validators.required, Validators.minLength(6), Validators.maxLength(16), Validators.pattern(/^[a-zA-Z]\w+$/)]) ],
+      password_confirm: ['', Validators.required ],
     });
   }
 
@@ -31,8 +34,13 @@ export class RegistrationModalComponent implements OnInit {
   }
 
   addIp() {
-    if (this.form.controls.ip.value > 0 && this.form.controls.ip.valid) {
+    if (this.form.controls.ip.value !== '' && this.form.controls.ip.valid) {
       this.ips = this.ips.concat(this.form.controls.ip.value);
+      this.form.patchValue({'ip': ''});
+      this.form.controls['ip'].markAsUntouched();
+      this.isIpEmpty = false;
+    } else {
+      this.isIpEmpty = true;
     }
 
   }
@@ -46,10 +54,16 @@ export class RegistrationModalComponent implements OnInit {
 
   submitForm(){
     console.log(this.form);
+
+    this.form.controls['name'].markAsTouched();
+    this.form.controls['email'].markAsTouched();
+    this.form.controls['ip'].markAsTouched();
+    this.form.controls['password'].markAsTouched();
+    this.form.controls['password_confirm'].markAsTouched();
+    this.noIps = this.ips.length < 1;
     if (this.form.valid) {
-      this.isMismatched = this.form.controls['password'].value === this.form.controls['password_confirm'].value;
-      this.noIps = this.ips.length > 0;
-      if (this.noIps && this.isMismatched) {
+      this.isMismatched = this.form.controls['password'].value !== this.form.controls['password_confirm'].value;
+      if (!this.noIps && !this.isMismatched) {
         const data = {
           name: this.form.controls.name.value,
           email: this.form.controls.email.value,
@@ -59,10 +73,15 @@ export class RegistrationModalComponent implements OnInit {
         console.log('FORM SUBMITTED AND SENT TO SERVER WITH VALUE: ', data);
         this.form.reset();
         this.ips = [];
+        this.closeForm();
       } else {
         return;
       }
     }
+  }
+
+  closeForm() {
+    this.close.emit();
   }
 
 
